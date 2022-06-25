3461a0f996785e7380ff7585bc7334c1eb28d910
Graeme Berry <g.berry1@yahoo.co.uk>
2022-06-25 22:31:18 +0100

    Created SpecialPrice class. Updated StockItem class to take in a SpecialPrice. Test still fails at this point.

--- Checkout/Models/StockItem.cs
+++ Checkout/Models/StockItem.cs
@@ -5,9 +5,19 @@ public class StockItem
     public string Code { get; }
     public decimal UnitPrice { get; }
 
+    public SpecialPrice? SpecialPrice { get; }
+
     public StockItem(string code, decimal unitPrice)
     {
         this.Code = code;
         this.UnitPrice = unitPrice;
+        this.SpecialPrice = null;
+    }
+
+    public StockItem(string code, decimal unitPrice, SpecialPrice specialPrice)
+    {
+        this.Code = code;
+        this.UnitPrice = unitPrice;
+        this.SpecialPrice = specialPrice;
     }
 }
