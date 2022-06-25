5dfb8c6950425b53959e08c794e6866f4aa38eb9
Graeme Berry <g.berry1@yahoo.co.uk>
2022-06-25 22:07:29 +0100

    Committing base project and base test project.

---
+++ Checkout.Tests/CheckoutTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace Checkout.Tests;
+public class CheckoutTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
